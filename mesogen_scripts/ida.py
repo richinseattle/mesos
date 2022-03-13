1da02c808dd2132d9beef20ef5156658e7907075
richinseattle <richinseattle@gmail.com>
2022-03-13 03:21:15 -0700

    update mesogen script for ida 7.7

--- mesogen_scripts/ida.py
+++ mesogen_scripts/ida.py
@@ -10,7 +10,7 @@ print("Analysis done, generating meso")
 
 image_base = idaapi.get_imagebase()
 
-input_name = GetInputFile()
+input_name = os.path.basename(idaapi.get_input_file_path())
 if len(ARGV) >= 4 and ARGV[1] == "cmdline":
     input_name = ARGV[3]
 
@@ -23,26 +23,26 @@ with open(filename, "wb") as fd:
     # Write record type 0 (module)
     # unsigned 16-bit module name
     # And module name
-    fd.write(struct.pack("<BH", 0, len(input_name)) + input_name)
+    fd.write(struct.pack("<BH", 0, len(input_name)) + bytes(input_name, "utf-8"))
 
     for funcea in Functions():
         funcname = get_func_off_str(funcea)
 
         # Write record type 1 (function)
         # Write unsigned 16-bit function name length and function name
-        fd.write(struct.pack("<BH", 1, len(funcname)) + funcname)
+        fd.write(struct.pack("<BH", 1, len(funcname)) + bytes(funcname, "utf-8"))
 
         # Write unsigned 64-bit offset of the function WRT the module base
         fd.write(struct.pack("<Q", funcea - image_base))
 
         blockoffs = bytearray()
         for block in idaapi.FlowChart(idaapi.get_func(funcea)):
-            if is_code(getFlags(block.startEA)):
+            if is_code(get_flags(block.start_ea)):
                 # Write signed 32-bit offset from base of function
-                blockoffs += struct.pack("<i", block.startEA - funcea)
+                blockoffs += struct.pack("<i", block.start_ea - funcea)
         
         # Unsigned 32-bit number of blocks
-        fd.write(struct.pack("<I", len(blockoffs) / 4))
+        fd.write(struct.pack("<I", int(len(blockoffs) / 4)))
         fd.write(blockoffs)
 
 # Rename .tmp file to actual name
